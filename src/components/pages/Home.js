61aa358198ac6e782aef3151090d88dc61543c8a
japerry911 <jack.e.perry@outlook.com>
2020-08-28 15:55:13 -0600

    boilerplate set u[

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>HOME!</h1>
+    </div>
+  );
+};
+
+export default Home;
