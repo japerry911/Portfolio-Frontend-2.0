dc3bcec32407064493099742e18912da10193d8e
japerry911 <jack.e.perry@outlook.com>
2020-08-28 17:11:58 -0600

    added linkedin svg logo to assets

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({}));
