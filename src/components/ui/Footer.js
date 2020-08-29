374cf1cdf99fd6f914bdef010a3ece9fb6a882f0
japerry911 <jack.e.perry@outlook.com>
2020-08-29 08:51:40 -0600

    icons for linkedin and github are actual svg files now instead of components, tested and work

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
-import linkedin from "../../assets/linkedin";
+import linkedin from "../../assets/linkedin.svg";
+import github from "../../assets/github.svg";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: "#fff",
+    height: "100px",
+  },
+}));
 
 const Footer = () => {
   const classes = useStyles();
 
-  return <footer className={classes.footer}>AHH</footer>;
+  return (
+    <footer className={classes.footer}>
+      <img src={linkedin} style={{ height: "4rem", width: "4rem" }} />
+      <img src={github} style={{ height: "4rem", width: "4rem" }} />
+    </footer>
+  );
 };
 
 export default Footer;
