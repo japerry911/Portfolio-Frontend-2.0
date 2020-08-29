63e3948e78e2526b52ced11d91b8978579474a34
japerry911 <jack.e.perry@outlook.com>
2020-08-29 09:36:31 -0600

    added button transition

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -30,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
     padding: "1rem",
     backgroundColor: "transparent",
     marginTop: "2rem",
+    transition: "background-color 500ms ease-in",
     "&:hover": {
       backgroundColor: theme.palette.common.green,
       color: theme.palette.common.black,
