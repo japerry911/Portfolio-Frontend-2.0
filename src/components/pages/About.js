2c7e73a506e31dcfae7a6c5607fd70bec0d746b3
japerry911 <jack.e.perry@outlook.com>
2020-08-29 09:38:38 -0600

    boilerplate set u[

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const About = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+};
+
+export default About;
