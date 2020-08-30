2a165b44604dc3463cba8324cf2aefe1d7518abd
japerry911 <jack.e.perry@outlook.com>
2020-08-29 18:29:49 -0600

    boilerplate set up

---
+++ src/components/pages/Blogposts.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const Blogposts = () => {
+  const classes = useStyles();
+};
+
+export default Blogposts;
